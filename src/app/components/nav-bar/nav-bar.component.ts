a79355c9c67f109b7e005b34c6dbf01bba3099a5
Ziadal3tar <ziadalattar22@gmail.com>
2023-09-01 18:17:09 +0300

    'update3'

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,52 +1,57 @@
 import { SharedService } from 'src/app/services/shared.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import * as $ from 'jquery'
-
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
   userLoggedIn: any;
-  userData: any
-  role:Boolean=false
-  constructor(private SharedService: SharedService, private router: Router) { }
+  userData: any;
+  role: Boolean = false;
+  constructor(private SharedService: SharedService, private router: Router) {}
   ngOnInit(): void {
     console.log(localStorage.getItem('role'));
 
     if (localStorage.getItem('role')) {
-this.role = true
+      this.role = true;
     }
     this.SharedService.isLoggedIn.subscribe((value) => {
       if (value == true) {
-        this.userLoggedIn = true
+        this.userLoggedIn = true;
       } else {
-        this.userLoggedIn = false
+        this.userLoggedIn = false;
       }
-    })
+    });
     this.SharedService.currentUserData.subscribe((data: any) => {
-      this.userData = data
-    })
+      this.userData = data;
+    });
     $(document).click(function (event) {
       var clickover = $(event.target);
-      var _opened = $(".navbar-collapse").hasClass("navbar-collapse collapse show");
-      if (_opened === true && !clickover.hasClass("navbar-toggler") && !clickover.hasClass("form-control")) {
-          $("button.navbar-toggler").click();
+      var _opened = $('.navbar-collapse').hasClass(
+        'navbar-collapse collapse show'
+      );
+      if (
+        _opened === true &&
+        !clickover.hasClass('navbar-toggler') &&
+        !clickover.hasClass('form-control')
+      ) {
+        $('button.navbar-toggler').click();
       }
-  });
+    });
   }
   logOut() {
     if (this.userLoggedIn == true) {
       localStorage.clear();
-      this.userLoggedIn = false
-      this.router.navigate(['/signUp'])
+      this.userLoggedIn = false;
+      this.router.navigate(['/signUp']);
     }
   }
   cart() {
-    this.SharedService.switchCartValue()
+    this.SharedService.switchCartValue();
   }
 }
 // declare var module : NodeModule;
